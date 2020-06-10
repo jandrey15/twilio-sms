e097a6ff54667bded7976e2a15a9ba46dd6ac289
John Andrey <andrey_201015@hotmail.com>
2020-06-10 15:43:36 -0500

    Added connection database

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('Hello world! Node.js')
+const { app } = require('./server')
+const { config } = require('./config/index')
+const MongoLib = require('./lib/mongo')
+
+// On port
+const port = config.port
+
+//Mongo
+const db = new MongoLib()
+db.connect()
+
+app.listen(port, () => {
+  console.log(`Listening http://localhost:${port}`)
+})
